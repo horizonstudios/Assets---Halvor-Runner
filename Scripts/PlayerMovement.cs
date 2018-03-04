c78a937a18dad5e308ceb6d93ed0726038ca6971
tobiaspreecha <craftyhorizonstudios@gmail.com>
2018-03-04 13:26:22 +0100

    Changes
    added new stuff and might deleted a empty folder and some useless stuff

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -2,15 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float PlayerSpeed = 10.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Controls
+        #region Horizontal - Vertical
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        gameObject.GetComponent<Animator>().SetBool("isWalking", Input.GetButtonDown("Horizontal"));
+        gameObject.GetComponent<Animator>().SetBool("isWalking", Input.GetButtonDown("Vertical"));
+        #endregion
+        #region Flip
+        //Animations
+        if (horizontal < -0.1f)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else if (horizontal > 0.1f)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        if (vertical < -0.1f)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipY = false;
+        }
+        else if (vertical > 0.1f)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipY = true;
+        }
+        #endregion
+
+        //Physics
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(horizontal * PlayerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(vertical * PlayerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.x);
+    }
 }
